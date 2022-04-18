0313d428e6a8605a18f368ef121f7407a398230c
Eric Fortis <efortis@gmail.com>
2022-04-18 13:28:18 -0400

    Fix drawing the interactive smooth curve from right to left

--- draw-interactive-connection.js
+++ draw-interactive-connection.js
@@ -1,13 +1,27 @@
+/**
+ * A Bezier curve between two Points for drawing smooth connections.
+ * Path "M:p1 C:p1Anchor p2Anchor p2"
+ */
+function describeCurve(x1, y1, x2, y2) {
+	const xAnchorDelta = ((Math.abs(y2 - y1) + Math.abs(x2 - x1)) >> 2) | 0
+	return `M${x1},${y1} C${x1 + xAnchorDelta},${y1} ${x2 - xAnchorDelta},${y2} ${x2},${y2}`
+}
+
+
 window.addEventListener('load', function () {
-	const target = document.getElementById("InteractiveConnectionTarget")
-	const ic = document.getElementById("InteractiveConnection")
+	const target = document.getElementById('InteractiveConnectionTarget')
+	const ic = document.getElementById('InteractiveConnection')
 
 	let xStart = 0
 	let yStart = 0
 
 	function onMove(event) {
 		event.preventDefault()
-		ic.setAttribute('d', describeLinkCurve(xStart, yStart, event.offsetX, event.offsetY))
+		
+		if (event.offsetX > xStart)
+			ic.setAttribute('d', describeCurve(xStart, yStart, event.offsetX, event.offsetY))
+		else
+			ic.setAttribute('d', describeCurve(event.offsetX, event.offsetY, xStart, yStart))
 	}
 
 	target.addEventListener('pointerdown', function (event) {
@@ -27,13 +41,3 @@ window.addEventListener('load', function () {
 		target.removeEventListener('pointermove', onMove)
 	}
 })
-
-
-/**
- * A Bezier curve between two Points for drawing Links.
- * Path "M:p1 C:p1Anchor p2Anchor p2"
- */
-function describeLinkCurve(x1, y1, x2, y2) {
-	const xAnchorDelta = ((Math.abs(y2 - y1) + Math.abs(x2 - x1)) >> 2) | 0
-	return `M${x1},${y1} C${x1 + xAnchorDelta},${y1} ${x2 - xAnchorDelta},${y2} ${x2},${y2}`
-}
