5fc5b3a5fef2b0d291feab9195f8ff6cf97ab26e
Eric Fortis <efortis@gmail.com>
2022-04-18 13:34:24 -0400

    lint

--- rel-noopener.js
+++ rel-noopener.js
@@ -1,6 +1,6 @@
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Link_types/noopener
 (function AddRelNoOpener() {
-	var links = document.querySelectorAll('a')
-	for (var i = 0; i < links.length; i++)
+	const links = document.querySelectorAll('a')
+	for (let i = 0; i < links.length; i++)
 		links[i].rel = 'noopener'
 }());
