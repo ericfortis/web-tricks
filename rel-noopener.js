d2b35c1b0c79dc53aa4523ada9181b5a55de9110
Eric Fortis <efortis@gmail.com>
2022-04-19 07:47:22 -0400

    embedding

--- rel-noopener.js
+++ rel-noopener.js
@@ -1,6 +1,4 @@
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Link_types/noopener
-(function AddRelNoOpener() {
-	const links = document.querySelectorAll('a')
-	for (let i = 0; i < links.length; i++)
-		links[i].rel = 'noopener'
-}());
+function injectRelNoOpener() {
+	document.querySelectorAll('a').forEach(link => link.rel = 'noopener')
+}
