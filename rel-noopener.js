dcc0a05ac392a0be0366490176c7ddb30f0658d8
Eric Fortis <efortis@gmail.com>
2022-04-19 08:49:38 -0400

    syntax highlight

--- rel-noopener.js
+++ rel-noopener.js
@@ -1,4 +1,5 @@
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Link_types/noopener
 function injectRelNoOpener() {
-	document.querySelectorAll('a').forEach(link => link.rel = 'noopener')
+	document.querySelectorAll('a')
+		.forEach(link => link.rel = 'noopener')
 }
