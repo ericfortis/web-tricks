fbed9906b26362ac6949c903b6fe608b0cc69c7d
Eric Fortis <efortis@gmail.com>
2022-05-12 20:30:24 -0400

    doc

--- pulse-on-change.js
+++ pulse-on-change.js
@@ -6,8 +6,11 @@
 	[numA, numB].forEach(el => el.addEventListener('change', function () {
 		output.innerText = parseFloat(numA.value) + parseFloat(numB.value)
 
+		// Look Here
 		output.classList.remove('feedbackPulse')
-		requestAnimationFrame(function () {
+		
+		// Then wait for the next repaint before adding it back
+		requestAnimationFrame(function () { 
 			output.classList.add('feedbackPulse')
 		})
 	}))
