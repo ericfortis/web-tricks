e710d01b8455d7727f1c5003edfc6868ed706582
Eric Fortis <efortis@gmail.com>
2022-06-11 16:57:11 -0400

    refactor

--- pulse-on-change.js
+++ pulse-on-change.js
@@ -1,17 +1,17 @@
-(function () {
+(() => {
 	const numA = document.getElementById('numA')
 	const numB = document.getElementById('numB')
 	const output = document.getElementById('outputAB');
 
-	[numA, numB].forEach(el => el.addEventListener('change', function () {
+	[numA, numB].forEach(el => el.addEventListener('change', () => {
 		output.innerText = numA.valueAsNumber + numB.valueAsNumber
 
 		// 👀 here
 		output.classList.remove('feedbackPulse')
-		
+
 		// Then wait for the next repaint before adding it back
-		requestAnimationFrame(function () { 
+		requestAnimationFrame(() => {
 			output.classList.add('feedbackPulse')
 		})
 	}))
-}());
+})();
