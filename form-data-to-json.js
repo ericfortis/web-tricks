1dcf3227cd61aada01e85d99b859b83172a113c7
Eric Fortis <efortis@gmail.com>
2024-09-05 21:22:55 -0400

    ref

--- form-data-to-json.js
+++ form-data-to-json.js
@@ -1,15 +1,9 @@
 function formDataToJSON(form) {
-	const data = {}
-	for (const [k, v] of new FormData(form))
-		data[k] = v
-	return JSON.stringify(data)
+	return Object.fromEntries(new FormData(form))
 }
 
 // Usage example:
-const myForm = null;
 fetch('/foo', {
 	body: formDataToJSON(myForm),
-	method: 'POST',
-	headers: new Headers({ 'Content-Type': 'application/json' }),
-	credentials: 'include'
+	method: 'POST'
 })
