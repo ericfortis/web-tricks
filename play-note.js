b1c70db7d55d868cdd083c57fb31d79111bf228b
Eric Fortis <efortis@gmail.com>
2025-06-15 21:59:39 -0400

    refactor playNote

--- play-note.js
+++ play-note.js
@@ -1,19 +1,16 @@
 // https://reeteshghimire.com.np/2020/03/27/play-musical-notes-with-javascript/
 function playNote(frequency = 329.6) {
 	const context = new AudioContext()
-	const on = context.createOscillator()
+	const osc = context.createOscillator()
 	const gn = context.createGain()
 	
-	on.type = 'sine'
-	on.connect(gn)
-	on.frequency.value = frequency
-	on.start(0)
+	osc.type = 'sine'
+	osc.connect(gn)
+	osc.frequency.value = frequency
+	osc.start(0)
+	osc.stop(1) // duration sec
 
 	gn.connect(context.destination)
 	gn.gain.setValueAtTime(0.1, context.currentTime) // volume
 	gn.gain.exponentialRampToValueAtTime(0.00001, context.currentTime + 1)
-	
-	setTimeout(function () {
-		on.stop()
-	}, 1000)
 }
